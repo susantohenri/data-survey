959cc25d1f3a8749aed5f3df894bc736174713c9
henrisusanto <henri@member.id>
2023-05-25 19:45:12 +0700

    initial commit

---
+++ data-survey.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Data Survey
+ *
+ * @package     DataSurvey
+ * @author      Henri Susanto
+ * @copyright   2022 Henri Susanto
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Data Survey
+ * Plugin URI:  https://github.com/susantohenri/data-survey
+ * Description: Plugin for managing question rotation of data survey
+ * Version:     1.0.0
+ * Author:      Henri Susanto
+ * Author URI:  https://github.com/susantohenri/
+ * Text Domain: DataSurvey
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
